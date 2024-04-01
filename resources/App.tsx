e6e9ab136e01ac8b5430f7426a384ba70c4a2b04
Cody Fincher <codyfincher@google.com>
2024-04-01 18:09:20 +0000

    chore: linting updates

--- resources/App.tsx
+++ resources/App.tsx
@@ -3,12 +3,12 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 
 const App: React.FC = () => {
-    return (
-        <ThemeProvider defaultTheme="light" storageKey="dma-ui-theme">
-            <Dashboard />
-            <Toaster />
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="dma-ui-theme">
+      <Dashboard />
+      <Toaster />
+    </ThemeProvider>
+  )
 }
 
 export default App
--- resources/lib/api.ts
+++ resources/lib/api.ts
@@ -1,17 +1,19 @@
-declare var apidata: any;
+declare var apidata: any
 
-export function invoicesRaw()  {
-    return apidata.filter((x: { domain: string; }) => x.domain == "invoices")[0].data
+export function invoicesRaw() {
+  return apidata.filter((x: { domain: string }) => x.domain == "invoices")[0]
+    .data
 }
 
 export function invoicesTotalRevenue() {
-    return invoicesRaw().reduce((a, b) => a + Number(b.totalAmount), 0)
+  return invoicesRaw().reduce((a, b) => a + Number(b.totalAmount), 0)
 }
 
 export function invoicesNumberSales() {
-    return invoicesRaw().length
+  return invoicesRaw().length
 }
 
-export function purchasesRaw()  {
-    return apidata.filter((x: { domain: string; }) => x.domain == "purchases")[0].data
+export function purchasesRaw() {
+  return apidata.filter((x: { domain: string }) => x.domain == "purchases")[0]
+    .data
 }
